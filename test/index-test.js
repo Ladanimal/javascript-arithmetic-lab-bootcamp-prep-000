1a6a931b221a602bca7d34c42e0a0d447de5ec66
ladanziaee@gmail.com <ladanziaee@gmail.com>
2018-02-23 23:04:03 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -28,8 +28,11 @@ it('inc(n) increments n and returns the result', function() {
   expect(inc(a)).toEqual(a + 1)
 })
 
-it('dec(n) decrements n and returns the result', function() {
+describe("dec", function () {
+it('dec(n) decrements n and returns the result', 
+  function() {
   expect(dec(a)).toEqual(a - 1)
+})
 })
 
 describe('makeInt(n)', function() {

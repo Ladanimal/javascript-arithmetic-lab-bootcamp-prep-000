f78a2f96a54aacd56124541f66ea622cc6f4c11b
ladanziaee@gmail.com <ladanziaee@gmail.com>
2018-02-23 22:35:27 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,12 +1,10 @@
-var a, b
-
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
+describe("add" function() {
+it('adds (a, b) two numbers and returns the result', 
+function() {
+  expect(add(a, b)
+  .toEqual(a + b)
 })
-
-it('add(a, b) adds two numbers and returns the result', function() {
-  expect(add(a, b)).toEqual(a + b)
+  
 })
 
 it('subtract(a, b) subtracts b from a and returns the result', function() {
